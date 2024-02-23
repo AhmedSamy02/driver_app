8c2d2225f9c6a69df9586beb23e563a762090f77
AhmedSamy02 <https://github.com/AhmedSamy02>
2024-02-23 18:13:54 +0200

    [📦️ PKG] Adding state managment package

---
+++ lib/cubits/finish_order_cubit/finish_order_state.dart
@@ -0,0 +1,6 @@
+class FinishOrderState{}
+class FinishOrderInitialState extends FinishOrderState {
+  
+}
+class FinishOrderFailedState extends FinishOrderState {}
+class FinishOrderSuccessState extends FinishOrderState {}
